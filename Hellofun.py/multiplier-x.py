d6a12aa3246828155083c645a465cad7d71bb5fb
MertcanYildirim078 <99068314@mydavinci.nl>
2021-11-10 11:40:20 +0100

    'tafels_bter'

---
+++ Hellofun.py/multiplier-x.py
@@ -0,0 +1,14 @@
+tafel = int(input('Welke tafel wil je zien? '))
+
+def tafels(tafel):
+    print(1 * tafel)
+    print(2* tafel)
+    print(3 * tafel)
+    print(4 * tafel)
+    print(5 * tafel)
+    print(6 * tafel)
+    print(7 * tafel)
+    print(8 * tafel)
+    print(9 * tafel)
+    print(10 * tafel)
+tafels(tafel)
