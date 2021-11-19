d34c8f76d4747cb8af9327079cd7ba505c39eefd
MertcanYildirim078 <99068314@mydavinci.nl>
2021-11-19 09:17:25 +0100

    ('multiplyer_verbeterd')

--- Hellofun.py/multiplier-x.py
+++ Hellofun.py/multiplier-x.py
@@ -1,14 +1,9 @@
 tafel = int(input('Welke tafel wil je zien? '))
 
 def tafels(tafel):
-    print(1 * tafel)
-    print(2* tafel)
-    print(3 * tafel)
-    print(4 * tafel)
-    print(5 * tafel)
-    print(6 * tafel)
-    print(7 * tafel)
-    print(8 * tafel)
-    print(9 * tafel)
-    print(10 * tafel)
+    for x in range(1,11):
+        totaal = x * tafel
+        print (totaal)
+        
+
 tafels(tafel)
