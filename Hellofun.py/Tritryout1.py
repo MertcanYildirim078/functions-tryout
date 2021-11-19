8739cd8a139088f9bafd187dbc07b9907c55f00b
MertcanYildirim078 <99068314@mydavinci.nl>
2021-11-19 10:15:55 +0100

    ('tritryout2')

---
+++ Hellofun.py/Tritryout1.py
@@ -0,0 +1,6 @@
+def Helloworld(repeat=1):
+    for x in range(repeat):
+        print('Hello World!')
+    
+
+Helloworld(3)
---
+++ Hellofun.py/Tritryout2.py
@@ -0,0 +1,19 @@
+def addition(number1=1, number2=1):
+    print(number1 + number2)
+def subtraction(number1=2, number2=1):
+    print(number1 - number2)
+def multiplication(number1=1, number2=1):
+    print(number1 * number2)
+def division(number1=2, number2=1):
+    print(number1 / number2)
+def increment(number):
+    print(number + 1)
+def decrement(number):
+    print(number - 1)
+addition(2,4)
+subtraction(4,2)
+multiplication(10*7)
+division(110,22)
+increment(111)
+decrement(2)
+
